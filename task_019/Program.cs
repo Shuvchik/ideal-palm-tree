0f38c471416772d9a1d8d60d145a4345f88f62cd
Shuvchik <ishuvs@gmail.com>
2022-11-01 22:46:33 +0300

    Решение задачи 19 с функцией

--- task_019/Program.cs
+++ task_019/Program.cs
@@ -4,15 +4,35 @@
 // 12821 -> да
 // 23432 -> да
 
+int Palindrom(int digit)
+{
+    int result = 0;
+    if (digit / 10000 == digit % 10 && (digit / 1000) % 10 == (digit % 100) / 10)
+    {
+        result = 1;
+    }
+    // else
+    // {
+    //     result = 2;
+    // }
+    return result;
+}
+
 Console.Clear();
 
 Console.Write("Введите пятизначное число: ");
-int a = Convert.ToInt32(Console.ReadLine());
+int digit = Convert.ToInt32(Console.ReadLine());
 
-if (a < 10000 || a > 99999)
+if (digit < 10000 || digit > 99999)
+{
+    Console.ForegroundColor = ConsoleColor.DarkRed;
     Console.WriteLine("Это не пятизначное. Попробуй ещё");
+    Console.ResetColor();
+}
 
-if (a / 10000 == a % 10 && (a / 1000) % 10 == (a % 100) / 10)
+int result = Palindrom(digit);
+
+if (result == 1)
 {
     Console.WriteLine("Палиндром");
 }
@@ -20,3 +40,13 @@ else
 {
     Console.WriteLine("НЕ палиндром");
 }
+// Console.WriteLine("Это не пятизначное. Попробуй ещё");
+
+// if (a / 10000 == a % 10 && (a / 1000) % 10 == (a % 100) / 10)
+// {
+//     Console.WriteLine("Палиндром");
+// }
+// else
+// {
+//     Console.WriteLine("НЕ палиндром");
+// }
